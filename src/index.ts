b44030dea0b23d1e5a0ee68b9597e96475369465
MedEBoudali <med.sneaky@gmail.com>
2020-10-28 23:26:11 +0000

    db created and add migration

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { MikroORM } from '@mikro-orm/core';
+import microConfig from './mikro-orm.config';
+import { Post } from './entities/Post';
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  const post = orm.em.create(Post, { title: 'my first post' });
+  await orm.em.persistAndFlush(post);
+};
+
+main();
