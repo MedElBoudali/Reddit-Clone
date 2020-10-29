c9d46d790eb7a3548a43614496c3acbfb5bb2e82
MedEBoudali <med.sneaky@gmail.com>
2020-10-29 00:05:39 +0000

    adding mikro orm and done for now

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,14 @@ import microConfig from './mikro-orm.config';
 import { Post } from './entities/Post';
 
 const main = async () => {
+  // Migrating table
   const orm = await MikroORM.init(microConfig);
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
+  await orm.getMigrator().up();
+  // Adding data to table
+  // const post = orm.em.create(Post, { title: 'my first post' });
+  // await orm.em.persistAndFlush(post);
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 
 main();
