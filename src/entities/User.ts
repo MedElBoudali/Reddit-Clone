da3174eb88e8198e1ae31208ca6387f13b2b849a
MedElBoudali <med.sneaky@gmail.com>
2020-11-17 19:33:41 +0000

    entities converted from mikro orm to typeorm

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -1,29 +1,35 @@
-import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
-import { Field, ObjectType } from 'type-graphql';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  Column
+} from 'typeorm';
+import {Field, ObjectType} from 'type-graphql';
 
 @ObjectType()
 @Entity()
 export class User {
   @Field()
-  @PrimaryKey()
+  @PrimaryGeneratedColumn()
   id!: number;
 
-  @Field(() => String)
-  @Property({ type: 'date' })
-  createdAt = new Date();
+  @Field()
+  @CreateDateColumn()
+  createdAt: Date;
 
-  @Field(() => String)
-  @Property({ type: 'date', onUpdate: () => new Date() })
+  @Field()
+  @UpdateDateColumn()
   updatedAt = new Date();
 
   @Field()
-  @Property({ type: 'text', unique: true })
+  @Column({ unique: true })
   username!: string;
 
   @Field()
-  @Property({ type: 'text', unique: true })
+  @Column({ unique: true })
   email!: string;
 
-  @Property({ type: 'text' })
+  @Column({})
   password!: string;
 }
